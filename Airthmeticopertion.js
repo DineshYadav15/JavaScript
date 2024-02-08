cb3f237d86beafe0fd6f6939bddeb0ff00e955d6
Dinesh Yadav <dineshyadav1518@gmail.com>
2024-02-08 17:58:55 +0530

    airthmetic opertion first example

---
+++ Airthmeticopertion.js
@@ -0,0 +1,12 @@
+let num=prompt("enter to numbers");
+{
+    if(num%2===0)
+    {
+        console.log("it is even numbers");
+
+    }
+    else
+    {
+        console.log("it is odd numbers");
+    }
+}
